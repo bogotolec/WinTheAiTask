31711bf3fdb314163455eb728772d2288a2e6757
bogotolec <bogotolec@inbox.ru>
2017-08-16 16:25:09 +0700

    Произведена реорганизация файлов и пространств имён

--- WinTheAITask/Games/GameResult.cs
+++ WinTheAITask/Games/GameResult.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinTheAITask
+namespace WinTheAITask.Games
 {
     public enum GameState { Win, Lose, ContinuePlaying } 
 
