d55d089ae0e1cb4247604ffd1e985294aaf57fcd
bogotolec <bogotolec@inbox.ru>
2017-08-16 23:01:28 +0700

    Алгоритм изменён на алгоритм Эйлера

--- WinTheAITask/Games/LabyrinthGame.cs
+++ WinTheAITask/Games/LabyrinthGame.cs
@@ -39,228 +39,189 @@ namespace WinTheAITask.Games
 
         private class Labyrinth
         {
-            public int Height { get; }
+            public int Height { get; set; }
             public int Width { get; }
 
-            private Cell[,] Map;
+            private List<Cell[]> RowList;
 
             public override string ToString()
             {
                 StringBuilder SB = new StringBuilder();
+                SB.Append('#', Width * 2 + 1);
+
                 for (int i = 0; i < Height; i++)
                 {
+                    SB.Append('\n');
+                    SB.Append('#');
+
                     for (int j = 0; j < Width; j++)
                     {
-                        SB.Append(Map[i, j]);
+                        if (RowList[i][j].HasRightBorder)
+                            SB.Append(" #");
+                        else
+                            SB.Append("  ");
                     }
-                    SB.Append('\n');
+
+                    SB.Append("\n#");
+
+                    if (i == Height - 1)
+                        SB.Append('#', Width * 2);
+                    else
+                        for (int j = 0; j < Width; j++)
+                        {
+                            if (RowList[i][j].HasBottomBorder)
+                                SB.Append("##");
+                            else
+                                SB.Append(" #");
+                        }
                 }
 
                 return SB.ToString();
             }
 
-            private Labyrinth(int Height, int Width)
+            private Labyrinth(int Width)
             {
-                if (Height % 2 == 0)
-                    Height++;
-                if (Width % 2 == 0)
-                    Width++;
-
-                this.Height = Height;
+                this.Height = 0;
                 this.Width = Width;
 
-                Map = new Cell[Height, Width];
+                RowList = new List<Cell[]>();
+            }
+
+            public static Labyrinth Generate(Int32 seed = 0)
+            {
+                if (seed <= 0)
+                    seed = (new Random()).Next(0, Int32.MaxValue);
+
+                int Height = 45 + (int)(seed % 10);
+                int Width = 15 + (int)(seed % 9);
+
+                Labyrinth Resutlt = new Labyrinth(Width);
 
                 for (int i = 0; i < Height; i++)
                 {
-                    for (int j = 0; j < Width; j++)
-                    {
-                        if ((i & 1) == 0 || (j & 1) == 0)
-                            Map[i, j] = new Cell(Cell.CellType.Wall, i, j);
-                        else
-                            Map[i, j] = new Cell(Cell.CellType.Empty, i, j);
-                    }
-                }
-
-                Map[1, 1].Type = Cell.CellType.Start;
-                Map[Height - 2, Width - 2].Type = Cell.CellType.Finish;
-            }
-
-            public static Labyrinth Generate(UInt32 seed = 0)
-            {
-                if (seed == 0)
-                    seed = (UInt32)(new Random()).Next(0, Int32.MaxValue);
-
-                int Height = 90 + (int)(seed % 10);
-                int Width = 30 + (int)(seed % 9);
-
-                Labyrinth Resutlt = new Labyrinth(Height, Width);
-
-                int Modifer = 1;
-                while (!Resutlt.IsPassable)
-                {
-                    Resutlt.DeleteNearestWall((seed / Modifer) % (Resutlt.Height - 2) + 1,
-                                               (seed / Modifer) % (Resutlt.Width - 2) + 1);
-                    Modifer++;
+                    Resutlt.AddRow(seed);
                 }
 
                 return Resutlt;
             }
 
-            public void DeleteNearestWall(long y, long x)
+            public void AddRow(Int64 Seed)
             {
-                Queue<Cell> queue = new Queue<Cell>();
-                bool[,] Used = new bool[Height, Width];
+                Seed *= Height + 1;
 
-                if ((y % 2 == 0 && x % 2 == 0) || (y % 2 == 1 && x % 2 == 1))
+                if (RowList.Count != 0)
                 {
-                    switch (((x + y) / 2) % 4)
+                    List<int> Multiplisities = new List<int>();
+                    
+                    // Add row
+                    RowList.Add(new Cell[Width]);
+                    for (int i = 0; i < Width; i++)
                     {
-                        case 0:
-                            x += 1; break;
-                        case 1:
-                            y += 1; break;
-                        case 2:
-                            x -= 1; break;
-                        case 3:
-                            y -= 1; break;
+                        RowList[Height][i] = new Cell(RowList[Height - 1][i]);
+                        RowList[Height][i].HasRightBorder = false;
+                        if (RowList[Height][i].HasBottomBorder)
+                            RowList[Height][i].Multiplisity = -1;
+                        else
+                            Multiplisities.Add(RowList[Height][i].Multiplisity);
+                        RowList[Height][i].HasBottomBorder = false;
+                    }
+
+                    // Give multiplicity
+                    int Multiplisity = 0;
+                    for (int i = 0; i < Width; i++)
+                    {
+                        if (RowList[Height][i].Multiplisity == -1)
+                        {
+                            while (Multiplisities.Contains(Multiplisity))
+                                Multiplisity++;
+                            RowList[Height][i].Multiplisity = Multiplisity;
+                            Multiplisity++;
+                        }
+                    }
+
+                    // Create right borders
+                    for (int i = 0; i < Width - 1; i++)
+                    {
+                        if ((Seed / (i + 1)) % 3 == 0 && (RowList[Height][i + 1].Multiplisity != RowList[Height][i].Multiplisity))
+                            RowList[Height][i + 1].Multiplisity = RowList[Height][i].Multiplisity; // Do not add border
+                        else
+                            RowList[Height][i].HasRightBorder = true; // Add border
+
+                    }
+
+                    // Create bottom borders
+                    for (int i = 0; i < Width; i++)
+                    {
+                        if ((Seed / (i + 1)) % 4 != 0 && !IsAlone(Height, RowList[Height][i].Multiplisity))
+                            RowList[Height][i].HasBottomBorder = true; // Add border
+                    }
+                }
+                else
+                {
+                    // Add row and give multiplisity
+                    RowList.Add(new Cell[Width]);
+                    for (int i = 0; i < Width; i++)
+                    {
+                        RowList[Height][i] = new Cell();
+                        RowList[Height][i].Multiplisity = i;
+                    }
+
+                    // Create right borders
+                    for (int i = 0; i < Width - 1; i++)
+                    {
+                        if ((Seed / (i + 1)) % 2 == 0)
+                            RowList[Height][i].HasRightBorder = true; // Add border
+                        else
+                            RowList[Height][i + 1].Multiplisity = RowList[Height][i].Multiplisity; // Do not add border
+
+                    }
+
+                    // Create bottom borders
+                    for (int i = 0; i < Width; i++)
+                    {
+                        if ((Seed / (i + 1)) % 4 != 0 && !IsAlone(Height, RowList[Height][i].Multiplisity))
+                            RowList[Height][i].HasBottomBorder = true; // Add border
                     }
                 }
 
-                if (y >= Height - 1)
-                    y -= 2;
-                if (x >= Width - 1)
-                    x -= 2;
-                if (y <= 0)
-                    y += 2;
-                if (x <= 0)
-                    x += 2;
+                RowList[Height][Width - 1].HasRightBorder = true;
 
-                for (int i = 0; i < Height; i++)
-                {
-                    for (int j = 0; j < Width; j++)
-                    {
-                        Used[i, j] = false;
-                    }
-                }
-
-                Cell NowCell = Map[y, x];
-                Used[y, x] = true;
-                
-                while (NowCell.Type != Cell.CellType.Wall)
-                {
-                    if (NowCell.Y + 2 < Height - 1 && !Used[NowCell.Y + 2, NowCell.X])
-                    {
-                        queue.Enqueue(Map[NowCell.Y + 2, NowCell.X]);
-                        Used[NowCell.Y + 2, NowCell.X] = true;
-                    }
-                    if (NowCell.Y - 2 > 0 && !Used[NowCell.Y - 2, NowCell.X])
-                    {
-                        queue.Enqueue(Map[NowCell.Y - 2, NowCell.X]);
-                        Used[NowCell.Y - 2, NowCell.X] = true;
-                    }
-                    if (NowCell.X + 2 < Width - 1 && !Used[NowCell.Y, NowCell.X + 2])
-                    {
-                        queue.Enqueue(Map[NowCell.Y, NowCell.X + 2]);
-                        Used[NowCell.Y, NowCell.X + 2] = true;
-                    }
-                    if (NowCell.X - 2 > 0 && !Used[NowCell.Y, NowCell.X - 2])
-                    {
-                        queue.Enqueue(Map[NowCell.Y, NowCell.X - 2]);
-                        Used[NowCell.Y, NowCell.X - 2] = true;
-                    }
-
-                    if (queue.Count == 0)
-                        return;
-                    if (queue.Count > 30)
-                        return;
-                    NowCell = queue.Dequeue();
-                }
-
-                NowCell.Type = Cell.CellType.Empty;
+                Height++;
             }
 
-            public bool IsPassable
+            private bool IsAlone(int Row, int Multiplisity)
             {
-                get
+                int Count = 0;
+
+                for (int i = 0; i < Width; i++)
                 {
-                    int[,] IntMap = new int[Height, Width];
-
-                    for (int i = 0; i < Height; i++)
-                    {
-                        for (int j = 0; j < Width; j++)
-                        {
-                            if (Map[i, j].Type == Cell.CellType.Wall)
-                                IntMap[i, j] = -1;
-                            else
-                                IntMap[i, j] = 0;
-                        }
-                    }
-
-                    IntMap[1, 1] = 1;
-
-                    bool HasNewCell = true;
-                    int Counter = 2;
-
-                    while (HasNewCell)
-                    {
-                        HasNewCell = false;
-
-                        for (int i = 1; i < Height - 1; i++)
-                        {
-                            for (int j = 1; j < Width - 1; j++)
-                            {
-                                if (IntMap[i, j] == 0 &&
-                                    (IntMap[i + 1, j] == Counter - 1 ||
-                                     IntMap[i - 1, j] == Counter - 1 ||
-                                     IntMap[i, j - 1] == Counter - 1 ||
-                                     IntMap[i, j - 1] == Counter - 1))
-                                {
-                                    IntMap[i, j] = Counter;
-                                    HasNewCell = true;
-                                }
-                            }
-                        }
-
-                        Counter++;
-                    }
-
-                    if (IntMap[Height - 2, Width - 2] != 0)
-                        return true;
-                    return false;
+                    if (RowList[Row][i].Multiplisity == Multiplisity)
+                        Count++;
                 }
+
+                if (Count == 1)
+                    return true;
+
+                return false;
             }
 
             private class Cell
             {
-                public int Y { get; }
-                public int X { get; }
                 public bool HasRightBorder { get; set; }
                 public bool HasBottomBorder { get; set; }
+                public int Multiplisity { get; set; }
 
-                public override string ToString()
+                public Cell()
                 {
-                    switch (Type)
-                    {
-                        case CellType.Empty:
-                            return " ";
-                        case CellType.Wall:
-                            return "#";
-                        case CellType.Teleport:
-                            return "0";
-                        case CellType.Start:
-                            return "s";
-                        case CellType.Finish:
-                            return " ";
-                        default:
-                            return null;
-                    }
+                    HasRightBorder = false;
+                    HasBottomBorder = false;
                 }
 
-                public Cell(int Y, int X)
+                public Cell(Cell ToClone)
                 {
-                    this.Y = Y;
-                    this.X = X;
+                    HasBottomBorder = ToClone.HasBottomBorder;
+                    HasRightBorder = ToClone.HasRightBorder;
+                    Multiplisity = ToClone.Multiplisity;
                 }
             }
         }
